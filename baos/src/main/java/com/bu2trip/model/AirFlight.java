4e5cabe1923dcde3b00159bc9abff0dcca5b4782
Root Liu <ewing@bu2trip.com>
2015-12-25 14:33:48 +0800

    移除保盛

--- baos/src/main/java/com/bu2trip/model/AirFlight.java
+++
@@ -1,5 +0,0 @@
-package com.bu2trip.model;
-
-public class AirFlight {
-
-}
--- baos/src/test/java/com/bu2trip/config/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package com.bu2trip.config;
-
-public class BaseTest {
-	public String username = "nick";
-	public String password = "nick";
-	public String deskey = "nick_baosheng_international_test";
-	public String agencyCode = "4561";
-}
